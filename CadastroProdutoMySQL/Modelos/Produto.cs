8ef30ea91d629a75ba4416ec742f9c682a3ad93e
Tony <antony.branquinho@gmail.com>
2025-07-01 16:51:22 -0300

    Primeiro commit: Criei as classes Produto e Estoque

---
+++ CadastroProdutoMySQL/Modelos/Produto.cs
@@ -0,0 +1,23 @@
+﻿namespace CadastroProdutoMySQL.Modelos
+{
+    public class Produto
+    {
+        // Atributos da classe
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        // Construtor vazio
+        public Produto()
+        {
+        }
+
+        // Construtor com parametros
+        public Produto(int codigo, string nome, double preco)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+}
---
+++ CadastroProdutoMySQL/Servicos/ProdutoServico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CadastroProdutoMySQL.Modelos;
+
+namespace CadastroProdutoMySQL.Servicos
+{
+    public class ProdutoServico
+    {
+        // Atributos da classe
+        public List<Produto> produtos { get; private set; }
+        public List<Estoque> estoque { get; private set; }
+
+        // Metodo vazio
+        public ProdutoServico() 
+        {
+        }
+
+        // Metodo com parametros
+        public ProdutoServico(List<Produto> produtos, List<Estoque> estoque)
+        {
+            this.produtos = produtos;
+            this.estoque = estoque;
+        }
+
+
+    }
+}
