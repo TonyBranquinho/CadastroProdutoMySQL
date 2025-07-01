ff7fda096e210782d3fbf17d0798bd9cb8ab9c14
Tony <antony.branquinho@gmail.com>
2025-07-01 16:51:22 -0300

    Primeiro commit: Criei as classes Produto e Estoque

---
+++ CadastroProdutoMySQL/Modelos/Estoque.cs
@@ -0,0 +1,18 @@
+﻿namespace CadastroProdutoMySQL.Modelos
+{
+    public class Estoque
+    {
+        public int Codigo { get; set; }
+        public int Quantidade { get; set; }
+
+        public Estoque()
+        {
+        }
+
+        public Estoque(int codigo, int quantidade)
+        {
+            Codigo = codigo;
+            Quantidade = quantidade;
+        }
+    }
+}
