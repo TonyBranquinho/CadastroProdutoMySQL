c190dc6de98514093f3c3ac6ead365b4693fafba
Tony <antony.branquinho@gmail.com>
2025-07-24 22:13:54 -0300

    Criei um CRUD na RepositoryCategoria

--- CadastroProdutoMySQL/Controllers/CategoriaController.cs
+++ CadastroProdutoMySQL/Controllers/CategoriaController.cs
@@ -1,6 +1,27 @@
-﻿namespace CadastroProdutoMySQL.Controllers
+﻿using CadastroProdutoMySQL.Dados;
+using CadastroProdutoMySQL.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Mysqlx.Connection;
+using MySqlX.XDevAPI;
+using System.Runtime.Intrinsics.X86;
+
+namespace CadastroProdutoMySQL.Controllers
 {
-    public class CategoriaController
+    [ApiController] // Diz que essa classe é um controlador de API
+    [Route("[controller]")] // Define o nome da classe como rota
+
+    /* Declara que essa classe herda todos os metodos e propriedades da 
+    ControllerBase que é a classe base da Microsoft para controller API */
+    public class CategoriaController : ControllerBase
     {
+
+        // Campo privado/ atribuido somente uma vez/ tipo campo / nome do campo - Instancia a classe de operaçoes do banco
+        private readonly RepositoryCategoria _operacoes;
+
+
+
+
+
     }
 }
